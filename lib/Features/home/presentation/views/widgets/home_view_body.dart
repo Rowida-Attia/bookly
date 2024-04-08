eaae3988596595405cbe15971940a165c2373c56
Rowida Attia <>
2024-04-09 01:22:35 +0200

    best seller item done

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,15 +1,8 @@
-import 'dart:js';
-
 import 'package:bookly/Features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featureed_list_view.dart';
-import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
