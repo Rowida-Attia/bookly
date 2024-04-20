ee72b5aee0eefe438c94e13736251fab79a82099
Rowida Attia <>
2024-04-21 00:48:07 +0200

    Create featured books method

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_book_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_book_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
+ 
 import 'package:equatable/equatable.dart';
 
 part 'featured_book_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
-  FeaturedBookCubit() : super(FeaturedBookInitial());
+  FeaturedBookCubit(this.homeRepo) : super(FeaturedBookInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBookLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBookFailure(failure.errMassage));
+    }, (books) {
+      emit(FeaturedBookSuccess(books));
+    });
+  }
 }
